d01ba74c396d3422711fe1c191486c0392221975
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-07 07:34:22 -0800

    islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# zinzinpolice
+
+"""Function that checks for lowercase characters."""
+
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
