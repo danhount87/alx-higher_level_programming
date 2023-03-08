5ce7cb626bc248e84049020a81f1ca8ed8d2dbbc
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-08 13:18:57 -0800

    ByteCode

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# zinzinpolice
+
+
+def magic_calculation(a, b, c):
+    """Python function that does exactly the same as
+    the following Python bytecode:"""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
