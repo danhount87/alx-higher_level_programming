8bd41acb8676f68d4b0e8d57099230be731da75d
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-09 03:36:25 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # zinzinpolice
 
-
 if __name__ == "__main__":
     """a program that prints the result
-of the addition of all arguments"""
-import sys
+    of the addition of all arguments"""
+    import sys
 
     sommation = 0
     for i in range(len(sys.argv) - 1):
