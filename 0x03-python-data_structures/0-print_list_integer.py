59598dce375d9c60db8b1593062b0e4411eede07
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-12 05:15:27 -0700

    Replace element

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# zinzinpolice
+
 def print_list_integer(my_list=[]):
+    '''function that prints all integers of a list.'''
     for j in range(len(my_list)):
         print("{:d}".format(my_list[j]))
