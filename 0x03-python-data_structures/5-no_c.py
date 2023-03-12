8f6a5cb72a4a251476ba6cba96db9d33921d7652
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-12 05:47:11 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@
 
 def no_c(my_string):
     '''function that removes all characters c and C from a string.'''
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    cop = [x for x in my_string if x != 'c' or x != 'C']
+    return ("".join(cop))
