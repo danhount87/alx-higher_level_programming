b8d3d40f0e5ba817ee2476146d8715e48c61c743
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-15 10:24:21 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-# zinzinpolice
+
 
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
-        return 0
-    man_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    man_n = 0
-    for k in range(len(roman_string)):
-        if k > 0 and man_d[roman_string[k]] > man_d[roman_string[k - 1]]:
-            man_n += man_d[roman_string[k]] - 2 * \
-                        man_d[roman_string[k - 1]]
+    """Converts a roman numeral to an integer."""
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return (0)
+
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    num = 0
+
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return (0)
+
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
+
         else:
-            man_n += man_d[man_string[k]]
-    return man_n
+            num += roman_dict[roman_string[i]]
+    return (num)
