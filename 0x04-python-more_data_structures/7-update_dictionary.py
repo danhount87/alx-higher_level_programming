52a6d11da593c830eff7312539cae14fd7cac562
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-15 09:31:40 -0700

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# zinzinpolice
+
+def update_dictionary(my_dict, key, value):
+    '''function that replaces or adds key/value in a dictionary.'''
+    my_dict[key] = value
+    return (my_dict)
