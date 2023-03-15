dadf65134c980f25a6f24fe94d32b85a554224fe
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-15 15:31:27 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer"""
     if (not isinstance(roman_string, str) or
         roman_string is None):
         return (0)
+
     roman_dict = {
             "I": 1,
             "V": 5,
@@ -12,7 +12,7 @@ def roman_to_int(roman_string):
             "C": 100,
             "D": 500,
             "M": 1000
-            }
+    }
     num = 0
 
     for i in range(len(roman_string)):
@@ -22,6 +22,7 @@ def roman_to_int(roman_string):
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
                 num += roman_dict[roman_string[i]] * -1
+
         else:
             num += roman_dict[roman_string[i]]
     return (num)
