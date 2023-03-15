b4f31a31d8d324466abbba0df97adbbede490338
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-15 09:20:20 -0700

    Only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# zinzinpolice
+
+def only_diff_elements(set_1, set_2):
+    '''function that returns a set of all
+    elements present in only one set.'''
+    return set_1 ^ set_2
