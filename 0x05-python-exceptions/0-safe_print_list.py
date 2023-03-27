d234a5b842250760da9893659cb57fa58de3813f
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-03-27 08:16:14 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#zinzinpolice
+def safe_print_list(my_list=[], x=0):
+    sat = 0
+    while True:
+        try:
+            if sat < x:
+                print(my_list[sat], end='')
+                sat += 1
+            else:
+                print()
+                return sat
+        except IndexError:
+            print()
+            return sat
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+We are learning in this project how awesome progamming in python is and differences btw errors and exceptions
