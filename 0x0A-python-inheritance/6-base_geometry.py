9e8941b5356c419c0e37a7454998eab933c18c36
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-04-11 09:35:29 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Public instance method: def area(self)."""
+
+    def area(self):
+        """function that raises an Exception with the message area() is not implemented."""
+        raise Exception("area() is not implemented")
