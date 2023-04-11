75f158dfc1cf4c28eea2e7882849b78abcdd5eb9
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-04-11 10:51:59 -0700

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""A Rectangle subclass Square."""
+"""
+A Rectangle subclass Square.
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(Rectangle):
-    """a class Square that inherits from Rectangle."""
+"""
+inherits from Rectangle.
+"""
 
+
+class Square(Rectangle):
+    """ Square Class """
     def __init__(self, size):
-        """print() should print, and str() should return,
-        the square description: [Square] <width>/<height>.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        """ size init"""
         self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
